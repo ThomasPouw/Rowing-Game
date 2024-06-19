b83f8aa11ade8e1c5f4b11d540aeb00fb728fb14
ThomasPouw <90248008+ThomasPouw@users.noreply.github.com>
2024-06-19 11:46:51 +0200

    .

--- Assets/Scripts/PedalBounds.cs
+++ Assets/Scripts/PedalBounds.cs
@@ -1,69 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
-using ProceduralToolkit;
 using UnityEngine;
 
 public class PedalBounds : MonoBehaviour
 {
     public Transform UpLeftPoint;
     public Transform DownRightPoint;
+    private Transform parent;
+    [SerializeField] private Vector3 BoxBound;
+    private Vector3 downPoint;
+    private Vector3 upPoint;
+    private Vector3 Up1;
+    private Vector3 Up2;
+    private Vector3 Up3;
+    private Vector3 Down1;
+    private Vector3 Down2;
+    private Vector3 Down3;
 
-//Go to the oriantation of the VR glasses and the turn 90 degrees to the right. That is the oriantation I am looking at.
+    //Go to the oriantation of the VR glasses and the turn 90 degrees to the right. That is the oriantation I am looking at.
     //[SerializeField] private bool isUpLeftXBigger;
     //[SerializeField] private bool isUpLeftYBigger;
     //[SerializeField] private bool isUpLeftZBigger;
-    
+
     // Start is called before the first frame update
     void Start()
     {
+        if(parent == null)
+            parent = transform.parent;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Compansate with rotation!
+        downPoint = parent.transform.rotation *(transform.position+ new Vector3(-BoxBound.x/2, -BoxBound.y/2, -BoxBound.z/2));
+        upPoint = parent.transform.rotation *(transform.position+ new Vector3(BoxBound.x/2, BoxBound.y/2, BoxBound.z/2));
+        //Get all the combinations :D
+        Up1 = parent.transform.rotation *(transform.position+ new Vector3(BoxBound.x/2, BoxBound.y/2, -BoxBound.z/2));
+        Up2 = parent.transform.rotation *(transform.position+ new Vector3(-BoxBound.x/2, BoxBound.y/2, -BoxBound.z/2));
+        Up3 = parent.transform.rotation *(transform.position+ new Vector3(-BoxBound.x/2, BoxBound.y/2, BoxBound.z/2));
+        Down1 = parent.transform.rotation *(transform.position+ new Vector3(BoxBound.x/2, -BoxBound.y/2, -BoxBound.z/2));
+        Down2 = parent.transform.rotation *(transform.position+ new Vector3(BoxBound.x/2, -BoxBound.y/2, BoxBound.z/2));
+        Down3 = parent.transform.rotation *(transform.position+ new Vector3(-BoxBound.x/2, -BoxBound.y/2, BoxBound.z/2));
     }
     public bool isPedalInBox(Transform pedalTransform)
     {
-        Debug.Log("X Condition: "+ (DownRightPoint.position.x > pedalTransform.position.x) + ", "+ (UpLeftPoint.position.x < pedalTransform.position.x));
-        Debug.Log("Y Condition: "+ (DownRightPoint.position.y < pedalTransform.position.y) + ", "+ (UpLeftPoint.position.y > pedalTransform.position.y));
-        Debug.Log("Z Condition: "+ (DownRightPoint.position.z > pedalTransform.position.z) + ", "+ (UpLeftPoint.position.z < pedalTransform.position.z));
-        //Debug.Log(pedalTransform.gameObject.name+" here!"+ pedalTransform.position+ "BoundBox: "+ UpLeftPoint.position+ " "+ DownRightPoint.position);
-        if(!(DownRightPoint.position.x > pedalTransform.position.x && UpLeftPoint.position.x < pedalTransform.position.x))
+        //Vector3 downPoint = parent.transform.rotation * DownRightPoint.position;
+        //Vector3 upPoint = parent.transform.rotation * UpLeftPoint.position;
+        //Debug.Log("X Condition: "+ (downPoint.x < pedalTransform.position.x) + ", "+ (upPoint.x > pedalTransform.position.x) + " "+ (downPoint.x < pedalTransform.position.x && upPoint.x > pedalTransform.position.x));
+        //Debug.Log("Y Condition: "+ (downPoint.y < pedalTransform.position.y) + ", "+ (upPoint.y > pedalTransform.position.y) + " "+ (downPoint.y < pedalTransform.position.y && upPoint.y > pedalTransform.position.y));
+       // Debug.Log("Z Condition: "+ (downPoint.z < pedalTransform.position.z) + ", "+ (upPoint.z > pedalTransform.position.z) + " "+ (downPoint.z < pedalTransform.position.z && upPoint.z > pedalTransform.position.z));
+       // Debug.Log(pedalTransform.gameObject.name+" here!"+ pedalTransform.position+ "BoundBox: "+ downPoint+ " "+ upPoint);
+        if(!(downPoint.x < pedalTransform.position.x && upPoint.x > pedalTransform.position.x))
             return false;
-        if(!(DownRightPoint.position.y < pedalTransform.position.y && UpLeftPoint.position.y > pedalTransform.position.y))
+        if(!(downPoint.y < pedalTransform.position.y && upPoint.y > pedalTransform.position.y))
             return false;
-        if(!(DownRightPoint.position.z > pedalTransform.position.z && UpLeftPoint.position.z < pedalTransform.position.z))
+        if(!(downPoint.z < pedalTransform.position.z && upPoint.z > pedalTransform.position.z))
             return false;
-        Debug.DrawLine(pedalTransform.position, pedalTransform.position + (Vector3.up *5), ColorE.green);
+        Debug.DrawLine(pedalTransform.position, pedalTransform.position + (Vector3.up *5), Color.green);
         return true;
     }
 
     private void OnDrawGizmos() {
-        Gizmos.color = Color.yellow;
-        //Get all the combinations :D
-        Vector3 Up1 = new Vector3(UpLeftPoint.position.x, UpLeftPoint.position.y, DownRightPoint.position.z);
-        Vector3 Up2 = new Vector3(DownRightPoint.position.x, UpLeftPoint.position.y, DownRightPoint.position.z);
-        Vector3 Up3 = new Vector3(DownRightPoint.position.x, UpLeftPoint.position.y, UpLeftPoint.position.z);
-        Vector3 Down1 = new Vector3(UpLeftPoint.position.x, DownRightPoint.position.y, DownRightPoint.position.z);
-        Vector3 Down2 = new Vector3(UpLeftPoint.position.x, DownRightPoint.position.y, UpLeftPoint.position.z);
-        Vector3 Down3 = new Vector3(DownRightPoint.position.x, DownRightPoint.position.y, UpLeftPoint.position.z);
-        
-        Gizmos.DrawLine(UpLeftPoint.position, Up1);
+        if(parent == null)
+        {
+            parent = transform.parent;
+        }
+ 
+
+        Gizmos.color = Color.magenta;
+
+        Gizmos.DrawLine(upPoint, Up1);
         Gizmos.DrawLine(Up1, Up2);
         Gizmos.DrawLine(Up2, Up3);
-        Gizmos.DrawLine(Up3, UpLeftPoint.position);
+        Gizmos.DrawLine(Up3, upPoint);
 
-        Gizmos.DrawLine(DownRightPoint.position, Down1);
+        Gizmos.DrawLine(downPoint, Down1);
         Gizmos.DrawLine(Down1, Down2);
         Gizmos.DrawLine(Down3, Down2);
-        Gizmos.DrawLine(Down3, DownRightPoint.position);
+        Gizmos.DrawLine(Down3, downPoint);
 
-        Gizmos.DrawLine(UpLeftPoint.position, Down2);
+        Gizmos.DrawLine(upPoint, Down2);
         Gizmos.DrawLine(Up1, Down1);
-        Gizmos.DrawLine(DownRightPoint.position, Up2);
+        Gizmos.DrawLine(downPoint, Up2);
         Gizmos.DrawLine(Down3, Up3);
-
-
     }
 }
